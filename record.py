7176f22537a2f1d953fcddbd69847eb62914ae5b
Dominic Davies <dominictdavies@gmail.com>
2025-05-06 00:01:22 +0800

    Use environment and refactor record

--- record.py
+++ record.py
@@ -1,14 +1,17 @@
+from dotenv import load_dotenv
 import sounddevice as sd
 import scipy.io.wavfile
+import os
 
 
-def record_audio(device_index, duration_seconds=60, sample_rate=48000):
+def record_audio(duration_seconds=60, sample_rate=48000):
+    load_dotenv()
     recording = sd.rec(
-        int(sample_rate * duration_seconds),
-        device=device_index,
+        device=int(os.getenv("DEVICE_INDEX")),
         samplerate=sample_rate,
         channels=2,
-        dtype="int16",
+        dtype="float32",
+        frames=int(duration_seconds * sample_rate),
     )
     sd.wait()
     scipy.io.wavfile.write("output.wav", sample_rate, recording)
